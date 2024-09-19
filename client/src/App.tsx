03628d225a5e5fa1cb8f2410f53adc77a53d7689
Hoàng An F8 <hoangan@fullstack.edu.vn>
2024-09-19 14:33:48 +0700

    ReactJS Socket - 03

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,9 +3,23 @@ import { io } from "socket.io-client";
 const socket = io("http://localhost:8080");
 export default function App() {
   const [title, setTitle] = useState("");
+  const handleClick = () => {
+    socket.emit(
+      "privateMessage",
+      {
+        type: "sendMessage",
+        data: {
+          userId: localStorage.getItem("user"),
+        },
+      },
+      socket.id
+    );
+  };
   useEffect(() => {
     socket.emit("message", { type: "init" });
     socket.on("message", ({ type, data }) => {
+      console.log(type, data);
+
       if (type === "init") {
         setTitle(data);
       }
@@ -17,6 +31,7 @@ export default function App() {
   return (
     <div>
       <h1>{title}</h1>
+      <button onClick={handleClick}>Click me</button>
     </div>
   );
 }
--- server/app.js
+++ server/app.js
@@ -8,6 +8,7 @@ const io = new Server(server, {
     origin: "http://localhost:5173",
   },
 });
+let currentSocketId = null;
 io.on("connection", (socket) => {
   socket.on("message", ({ type, data }) => {
     if (type === "init") {
@@ -15,6 +16,28 @@ io.on("connection", (socket) => {
         type: "init",
         data: "Chào mừng bạn đến với Unicode",
       });
+    }
+  });
+  socket.on("privateMessage", ({ type, data: { userId } }, socketId) => {
+    if (type === "sendMessage") {
+      console.log("Client vừa gửi tin nhắn", socketId, userId);
+      socket.emit("message", {
+        type: "newMessage",
+        data: "Tin nhắn 1",
+      });
+      io.emit("message", {
+        type: "newMessage",
+        data: "Tin nhắn tất cả nhận được",
+      });
+      if (+userId === 2) {
+        currentSocketId = socketId;
+      }
+      if (+userId === 1) {
+        socket.to(currentSocketId).emit("message", {
+          type: "newMessage",
+          data: "Tin nhắn 2",
+        });
+      }
     }
   });
   socket.on("disconnect", () => {
