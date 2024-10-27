621882609215acb98ecfacb5fd356791dc204220
Hoàng An F8 <hoangan@fullstack.edu.vn>
2024-10-27 16:13:38 +0700

    ReactJS Socket - 06

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,29 @@
 import Pusher from "pusher-js";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 export default function App() {
+  const [data, setData] = useState("");
   useEffect(() => {
     const pusher = new Pusher("eecdbb5ead2816c0516d", {
       cluster: "ap1",
     });
     const channel = pusher.subscribe("my-channel");
     channel.bind("my-event", (data) => {
-      console.log(data);
+      setData(data.message);
     });
     return () => {
       channel.unbind_all();
       pusher.unsubscribe("my-channel");
     };
   }, []);
-  return <div>App</div>;
+  const handleClick = async () => {
+    const response = await fetch("http://localhost:8080/messages");
+    console.log(response.ok);
+  };
+  return (
+    <div>
+      <h1>Hello</h1>
+      <h2>{data}</h2>
+      <button onClick={handleClick}>Click me</button>
+    </div>
+  );
 }
